cf347223ff7e6dd0f09c77ba06a3e2618b7ce2d1
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-04 15:14:22 +0800

    ✨ Added grid guides and initialize header component

---
+++ components/Header/Header.js
@@ -0,0 +1,14 @@
+/*
+***************************************
+Component: Header
+Author: Joimee
+Description: Layout &gt; Header
+***************************************
+*/
+import './Header.scss';
+
+export default () => (
+  <div className="header">
+    <h1>This is a Header component!</h1>
+  </div>
+);
--- styleguide/component-list.js
+++ styleguide/component-list.js
@@ -1,10 +1,17 @@
 // list of components
+import Header from 'components/Header/Header.styleguide';
+
 import Card from 'components/card/card.styleguide';
 
 import Button from 'components/button/button.styleguide';
 
 
 export default [
+  {
+    name: 'Header',
+    component: Header,
+  },
+
   {
     name: 'Card',
     component: Card,
