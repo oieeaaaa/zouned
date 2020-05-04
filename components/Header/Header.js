24194fd7d5b8d6f2d55393e3072e048f0ab4a1db
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-04 17:55:54 +0800

    ✨ Add header styles and markup

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,13 +2,32 @@
 ***************************************
 Component: Header
 Author: Joimee
-Description: Layout &gt; Header
+Description: Layout > Header
 ***************************************
 */
-import './Header.scss';
+import { useState } from 'react';
+import Icon from 'components/Icon/Icon';
 
-export default () => (
-  <div className="header">
-    <h1>This is a Header component!</h1>
-  </div>
-);
+export default () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => setIsNavOpen(!isNavOpen);
+  const onPlay = () => setIsPlaying(!isPlaying);
+
+  return (
+    <div className="header">
+      <div className="grid">
+        <div className="header__brand">zouned</div>
+        <div className="header-buttons">
+          <button className="header__btn" type="button" onClick={toggleNav}>
+            <Icon src="menu-icon.svg" />
+          </button>
+          <button className="header__btn" type="button" onClick={onPlay}>
+            <Icon src={isPlaying ? 'pause-icon.svg' : 'play-icon.svg'} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
