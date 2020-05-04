130731442f0bb4e4bdbd308ee46930d1a162219f
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-04 17:56:54 +0800

    🔥 Removed duplicate imports

--- components/button/button.js
+++ components/button/button.js
@@ -5,8 +5,6 @@ Author: joimee
 Description:
 ***************************************
 */
-import './button.scss';
-
 export default () => (
   <div className="button">
     <h1>This is a button component!</h1>
