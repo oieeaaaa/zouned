44ac3589071169ab2d0d91bc22c5891fddef4269
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-09 11:40:02 +0800

    ✨ INIT SERVER

---
+++ api/config/database.js
@@ -0,0 +1,16 @@
+module.exports = {
+  production: {
+    username: 'root',
+    password: '',
+    database: 'zouned',
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+  development: {
+    username: 'root',
+    password: '',
+    database: 'zouned',
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+};
---
+++ api/migrations/20200508223421-create-songs.js
@@ -0,0 +1,25 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Songs', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    title: {
+      type: Sequelize.STRING,
+    },
+    description: {
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface) => queryInterface.dropTable('Songs'),
+};
