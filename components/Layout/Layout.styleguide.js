de96c30840610ce30c6116642aa0cc7a13aa799a
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-10 20:29:49 +0800

    ✨ Add layout

---
+++ components/Layout/Layout.styleguide.js
@@ -0,0 +1,15 @@
+/*
+*******************************
+Stylguide for Layout.
+
+NOTE: Not for production use
+*******************************
+*/
+
+import Layout from './Layout';
+
+export default () => (
+  <Layout title="Sample Layout">
+    <h1>This is a sample layout</h1>
+  </Layout>
+);
