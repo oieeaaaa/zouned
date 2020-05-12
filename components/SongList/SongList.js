b68d0383499d2f06fdbcf5e5c90a6b4ccf59fec4
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-13 05:57:09 +0800

    ✨ Updated SongList component

--- components/SongList/SongList.js
+++ components/SongList/SongList.js
@@ -1,3 +1,4 @@
+/* eslint jsx-a11y/click-events-have-key-events: "off" */
 /*
 ***************************************
 Component: SongList
@@ -5,21 +6,38 @@ Author: Joimee
 Description: Display list of songs
 ***************************************
 */
-export default ({ list }) => (
-  <div className="song-list">
-    <div className="song-list-header">
-      <span className="song-list-header__item">no.</span>
-      <span className="song-list-header__item">title</span>
-      <span className="song-list-header__item">duration</span>
+import { useContext } from 'react';
+import PlayerContext from 'js/contexts/player';
+import convertTime from 'js/helpers/convertTime';
+
+export default ({ list, onPlay }) => {
+  const { activeSong } = useContext(PlayerContext);
+
+  return (
+    <div className="song-list">
+      <div className="song-list-header">
+        <span className="song-list-header__item">no.</span>
+        <span className="song-list-header__item">title</span>
+        <span className="song-list-header__item">duration</span>
+      </div>
+      <ul className="song-list-body">
+        {list.map(song => (
+          <li
+            className={`song-list-body__item${song.id === activeSong.id ? ' song-list-body__item--active' : ''}`}
+            key={song.id}
+          >
+            <p className="song-list__text">{song.id}</p>
+            <button
+              className="song-list__text song-list__text--center"
+              type="button"
+              onClick={() => onPlay(song)}
+            >
+              {song.title}
+            </button>
+            <p className="song-list__text">{convertTime(song.duration)}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-    <ul className="song-list-body">
-      {list.map(({ id, title }) => (
-        <li className="song-list-body__item" key={id}>
-          <p className="song-list__text">{id}</p>
-          <p className="song-list__text song-list__text--center">{title}</p>
-          <p className="song-list__text">--</p>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+  );
+};
