82be598544dda66a490870ce1bae02bae7910af3
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-14 05:39:57 +0800

    ✨ Added search functionality

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -5,13 +5,16 @@ Author: Joimee
 Description:
 ***************************************
 */
-export default ({ onSubmit }) => (
+export default ({ onSubmit, onChange, value }) => (
   <form className="search" onSubmit={onSubmit}>
     <div className="search-box">
       <input
         className="search__input"
+        name="search"
         type="search"
         placeholder="e.g., zen"
+        onChange={onChange}
+        value={value}
         required
       />
       <button className="search__btn" type="submit">Search</button>
