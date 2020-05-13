821549fdba50e72549252a3bd672251c5a3b4aff
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-14 05:56:10 +0800

    ✨ Initialize carousel component

---
+++ components/Carousel/Carousel.js
@@ -0,0 +1,12 @@
+/*
+***************************************
+Component: Carousel
+Author: Joimee
+Description: 
+***************************************
+*/
+export default () => (
+  <div className="carousel">
+    <h1>This is a Carousel component!</h1>
+  </div>
+);
---
+++ components/Carousel/Carousel.styleguide.js
@@ -0,0 +1,13 @@
+/*
+*******************************
+Stylguide for Carousel.
+
+NOTE: Not for production use
+*******************************
+*/
+
+import Carousel from './Carousel';
+
+export default () => (
+  <Carousel />
+);
