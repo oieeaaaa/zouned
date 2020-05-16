9f865532853f5ee28922470d49af44250926c4aa
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-17 05:52:40 +0800

    ✨ Added carousel component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,11 +2,29 @@
 ***************************************
 Component: Carousel
 Author: Joimee
-Description: 
+Description:
 ***************************************
 */
-export default () => (
-  <div className="carousel">
-    <h1>This is a Carousel component!</h1>
-  </div>
-);
+import Flickity from 'react-flickity-component';
+
+export default ({ list, activeIndex }) => {
+  const options = {
+    contain: true,
+    initialIndex: activeIndex,
+  };
+
+  return (
+    <Flickity
+      className="carousel"
+      options={options}
+      elementType="div"
+      static
+    >
+      {list.map(listItem => (
+        <div className="carousel__item" key={listItem.name}>
+          <img src={listItem.img} alt={listItem.name} />
+        </div>
+      ))}
+    </Flickity>
+  );
+};
--- components/Carousel/Carousel.styleguide.js
+++ components/Carousel/Carousel.styleguide.js
@@ -8,6 +8,30 @@ NOTE: Not for production use
 
 import Carousel from './Carousel';
 
+const carouselList = [
+  {
+    name: 'Wonder Boy',
+    img: 'https://bit.ly/2zCM6iI',
+  },
+  {
+    name: 'Cat',
+    img: 'https://bit.ly/35XkSiZ',
+  },
+  {
+    name: 'White Flower',
+    img: 'https://bit.ly/3dLDo0G',
+  },
+];
+
 export default () => (
-  <Carousel />
+  <div className="carousel--styleguide">
+    <Carousel list={carouselList} />
+    <style jsx>
+      {`
+          .carousel--styleguide {
+            grid-column: 2 / -2;
+          }
+      `}
+    </style>
+  </div>
 );
