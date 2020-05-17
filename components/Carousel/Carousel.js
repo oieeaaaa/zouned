1914ac0782012c5fd1f077b575a3644f70234a9a
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-18 07:52:15 +0800

    🔥 Updated carousel component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -7,10 +7,10 @@ Description:
 */
 import Flickity from 'react-flickity-component';
 
-export default ({ list, activeIndex }) => {
+export default ({ list, initialIndex, ...rest }) => {
   const options = {
     contain: true,
-    initialIndex: activeIndex,
+    initialIndex,
   };
 
   return (
@@ -19,10 +19,11 @@ export default ({ list, activeIndex }) => {
       options={options}
       elementType="div"
       static
+      {...rest}
     >
       {list.map(listItem => (
-        <div className="carousel__item" key={listItem.name}>
-          <img src={listItem.img} alt={listItem.name} />
+        <div className="carousel__item" key={listItem.title}>
+          <img src={listItem.imgSrc} alt={listItem.title} />
         </div>
       ))}
     </Flickity>
