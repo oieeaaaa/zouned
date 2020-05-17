4b2a3dff06e6ab757ad565f5fdc5500868c5ccd0
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-18 07:52:37 +0800

    🔥 Updated header component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,28 +5,31 @@ Author: Joimee
 Description: Layout > Header
 ***************************************
 */
-import { useState } from 'react';
+import Link from 'next/link';
+import { useContext } from 'react';
 import Icon from 'components/Icon/Icon';
+import PlayerContext from 'js/contexts/player';
 
 export default () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [isNavOpen, setIsNavOpen] = useState(false);
-
-  const toggleNav = () => setIsNavOpen(!isNavOpen);
-  const onPlay = () => setIsPlaying(!isPlaying);
+  const { activeSong } = useContext(PlayerContext);
 
   return (
     <div className="header">
       <div className="grid">
-        <div className="header__brand">zouned</div>
-        <div className="header-buttons">
-          <button className="header__btn" type="button" onClick={toggleNav}>
-            <Icon src="menu-icon.svg" />
-          </button>
-          <button className="header__btn" type="button" onClick={onPlay}>
-            <Icon src={isPlaying ? 'pause-icon.svg' : 'play-icon.svg'} />
-          </button>
-        </div>
+        <Link href="/">
+          <a className="header__brand">
+            zouned
+          </a>
+        </Link>
+        {activeSong.id && (
+          <div className="header-buttons">
+            <Link href="/play-queue/[id]" as={`/play-queue/${activeSong.id}`}>
+              <a className="header__btn">
+                <Icon src="menu-icon.svg" />
+              </a>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
