01bfde4af1b44e020d6337ecb5c14f9e9a0bb601
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-21 06:40:52 +0800

    ✨ Home is not supported on tablet

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const homeReducer = (state, action) => {
     case SET_SEARCH:
       return ({
         ...state,
-        songs: action.search,
+        search: action.search,
       });
     case SET_CATEGORIES:
       return ({
