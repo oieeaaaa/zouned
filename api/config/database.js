d9d8ee645696ad38238e080e7ddadf25ecbc6397
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-25 10:03:31 +0800

    🍱 Updated assets

--- api/config/database.js
+++ api/config/database.js
@@ -1,16 +1,16 @@
 module.exports = {
   production: {
-    username: 'root',
-    password: '',
-    database: 'zouned',
-    host: 'localhost',
+    username: 'prwwqkeda7n6qitk',
+    password: 'j6wil4vw58941fno',
+    database: 'fs2m39dnq5ixopv2',
+    host: 'pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
   },
   development: {
-    username: 'root',
-    password: '',
-    database: 'zouned',
-    host: 'localhost',
+    username: 'prwwqkeda7n6qitk',
+    password: 'j6wil4vw58941fno',
+    database: 'fs2m39dnq5ixopv2',
+    host: 'pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
   },
 };
