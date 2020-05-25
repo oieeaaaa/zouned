8e3e64b817a6eaabe898a884752f4e5dbb4aeeb1
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-25 09:31:28 +0800

    🐛 Fixed: DB config

--- api/db.js
+++ api/db.js
@@ -1,37 +1,9 @@
 const Sequelize = require('sequelize');
 
-const dev = process.env.NODE_ENV !== 'production';
-
-let config = {
-  name: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASS,
+// db setup
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DRIVER,
-};
-
-if (!dev) {
-  config = {
-    name: process.env.DB_PROD_NAME,
-    user: process.env.DB_PROD_USER,
-    pass: process.env.DB_PROD_PASS,
-    host: process.env.DB_PROD_HOST,
-    dialect: process.env.DB_PROD_DRIVER,
-  };
-}
-
-const {
-  name,
-  user,
-  pass,
-  host,
-  dialect,
-} = config;
-
-// db setup
-const db = new Sequelize(name, user, pass, {
-  host,
-  dialect,
 });
 
 module.exports = db;
