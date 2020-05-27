df7dce39969d45df149a5b596dbe2fba99c2b06f
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-05-28 06:09:57 +0800

    ✨🔥 Updated seeders and setup cloudinary

--- api/modules/songs/songs.js
+++ api/modules/songs/songs.js
@@ -1,16 +1,13 @@
 const { Op } = require('sequelize');
 const formidable = require('formidable');
-const path = require('path');
 const has = require('lodash.has');
 const mm = require('music-metadata');
+const cloudinary = require('../../cloudinary');
 const { Songs, Categories } = require('../../models');
 const getLastWeek = require('../../../js/helpers/getLastWeek');
 
-const assetsDir = path.resolve(__dirname, '../../../public/assets');
-
 // GET -- with Sounds of the Week
 const getSoundsOfTheWeek = async (req, res) => {
-  console.log(getLastWeek.toString());
   const data = await Songs.findAll({
     where: {
       createdAt: {
@@ -60,31 +57,33 @@ const getSongs = async (req, res) => {
 const addSong = (req, res, next) => {
   const form = formidable();
 
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      next(err);
+  form.parse(req, async (formError, fields, files) => {
+    if (formError) {
+      next(formError);
       return;
     }
 
     const { format } = await mm.parseFile(files.songSrc.path);
 
-    const data = {
-      ...fields,
-      categoryID: parseInt(fields.categoryID, 10),
-      songSrc: `/assets/${files.songSrc.name}`,
-      imgSrc: `/assets/${files.imgSrc.name}`,
-      duration: format.duration,
-    };
+    try {
+      const uploadedSong = await cloudinary.uploader.upload(files.songSrc.path, { resource_type: 'auto' });
+      const uploadedImage = await cloudinary.uploader.upload(files.imgSrc.path);
 
-    const newData = await Songs.create(data);
+      const data = {
+        ...fields,
+        categoryID: parseInt(fields.categoryID, 10),
+        songSrc: uploadedSong.url,
+        imgSrc: uploadedImage.url,
+        duration: format.duration,
+      };
 
-    res.send(newData);
-  })
-    // store file in /public/assets directory
-    .on('fileBegin', (name, file) => {
-      console.log({ assetsDir, fileName: file.name });
-      file.path = `${assetsDir}/${file.name}`;
-    });
+      const newData = await Songs.create(data);
+
+      res.send(newData);
+    } catch (fileUploadError) {
+      res.send(fileUploadError);
+    }
+  });
 };
 
 // UPDATE
@@ -100,36 +99,35 @@ const editSong = (req, res, next) => {
     const data = fields;
     const fileKeys = Object.keys(files);
 
-    // check if there is any file paths need to be updated
-    if (fileKeys.length !== 0) {
-      fileKeys.forEach(key => {
-        data[key] = `/assets/${files[key].name}`;
-      });
+    try {
+      // check if there is any file paths need to be updated
+      if (fileKeys.length !== 0) {
+        await fileKeys.forEach(async key => {
+          const uploadedFile = await cloudinary.uploader.upload(fields[key].path, { resource_type: 'auto' });
+          data[key] = uploadedFile.url;
+        });
+      }
+
+      // update song duration too (if songSrc exists in the payload)
+      if ('songSrc' in files) {
+        const { format } = await mm.parseFile(files.songSrc.path);
+        data.duration = format.duration;
+      }
+
+      // check if payload contains categoryID
+      if (has(data, 'categoryID')) {
+        data.categoryID = parseInt(data.categoryID, 10);
+      }
+
+      await Songs.update(data, { where: { id: data.id } });
+
+      const updatedSong = await Songs.findOne({ where: { id: data.id } });
+
+      res.send(updatedSong);
+    } catch (fieldsError) {
+      res.send(fieldsError);
     }
-
-    // update song duration too (if songSrc exists in the payload)
-    if ('songSrc' in files) {
-      const { format } = await mm.parseFile(files.songSrc.path);
-
-      data.duration = format.duration;
-    }
-
-    // check if payload contains categoryID
-    if (has(data, 'categoryID')) {
-      data.categoryID = parseInt(data.categoryID, 10);
-    }
-
-    await Songs.update(data, { where: { id: data.id } });
-
-    const updatedSong = await Songs.findOne({ where: { id: data.id } });
-
-    res.send(updatedSong);
-  })
-    // store file in /public/assets directory
-    .on('fileBegin', (name, file) => {
-      if (!file) return false;
-      file.path = `${assetsDir}/${file.name}`;
-    });
+  });
 };
 
 // DELETE
--- api/seeders/20200509032731-demo-songs.js
+++ api/seeders/20200509032731-demo-songs.js
@@ -1,50 +1,5 @@
 module.exports = {
-  up: (queryInterface) => queryInterface.bulkInsert('Songs', [
-    {
-      title: 'Rain',
-      description: 'N / A',
-      artist: 'Joimee',
-      categoryId: 4,
-      songSrc: '/assets/Rain.m4a',
-      imgSrc: '/assets/guillaume-bourdages-m8q2I_jPYzI-unsplash.jpg',
-      duration: 90.25596371882087,
-      updatedAt: new Date(),
-      createdAt: new Date(),
-    },
-    {
-      title: 'Bad Behaviors',
-      description: 'N / A',
-      artist: 'Ezequel Marotte',
-      categoryId: 3,
-      songSrc: '/assets/Bad Behaviors _ www.wowa.me.mp3',
-      imgSrc: '/assets/bad-behaviors.jpg',
-      duration: 560,
-      updatedAt: new Date(),
-      createdAt: new Date(),
-    },
-    {
-      title: 'Good God',
-      description: 'N / A',
-      artist: 'Wowa',
-      categoryId: 1,
-      songSrc: '/assets/Good God _ www.wowa.me.mp3',
-      imgSrc: '/assets/good-god.jpg',
-      duration: 100,
-      updatedAt: new Date(),
-      createdAt: new Date(),
-    },
-    {
-      title: 'Hangtime',
-      description: 'N / A',
-      artist: 'Wowa',
-      categoryId: 2,
-      songSrc: '/assets/Hangtime - unminus.com.mp3',
-      imgSrc: '/assets/hangtime.jpg',
-      duration: 183,
-      updatedAt: new Date(),
-      createdAt: new Date(),
-    },
-  ], {}),
+  up: (queryInterface) => queryInterface.bulkInsert('Songs', [], {}),
   /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
